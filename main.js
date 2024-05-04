0f43e714e85ab9e746f540430c9825b059ba9e4e
Rhythm Munshi <programmingwithrm@gmail.com>
2024-05-04 12:27:34 +0600

    Simple change:35

--- main.js
+++ main.js
@@ -492,7 +492,7 @@ document.getElementById("verifyButton").addEventListener("click", function () {
        
     // Timer initialization
     var timerDisplay = document.getElementById("timer");
-    var timeLeft = 5;
+    var timeLeft = 90;
 
     function countdown() {
         timerDisplay.textContent = timeLeft;
